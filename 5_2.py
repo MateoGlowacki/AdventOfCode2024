9f8dfb17aa36ad75ee615199327acf4a471f2767
MateoGlowacki <glowacki.mateo@gmail.com>
2024-12-06 00:53:50 +0100

    dag 5

--- 5_2.py
+++ 5_2.py
@@ -32,64 +32,79 @@ for line in lines:
     if correct == False:
         lines2.append(line)
 
-rules2 = []
-for n in range(len(rules)):    
-    if n != 0:            
-        if rules[n][0] < rules2[0][0]:
-            rules2.insert(0,rules[n])
-        else:
-            for m in range(len(rules2)):
+# rules2 = []
+# for n in range(len(rules)):    
+#     if n != 0:            
+#         if rules[n][0] < rules2[0][0]:
+#             rules2.insert(0,rules[n])
+#         else:
+#             for m in range(len(rules2)):
                 
-                if rules[n][0] == rules2[m][0]:
-                    if rules[n][1] < rules2[m][1]:
-                        rules2.insert(m,rules[n])
-                        break
-                    elif m+1 == len(rules2):
-                        rules2.append(rules[n])
-                        break
-                    elif rules[n][0] < rules2[m+1][0]:
-                        rules2.insert(m+1,rules[n])
-                        break
+#                 if rules[n][0] == rules2[m][0]:
+#                     if rules[n][1] < rules2[m][1]:
+#                         rules2.insert(m,rules[n])
+#                         break
+#                     elif m+1 == len(rules2):
+#                         rules2.append(rules[n])
+#                         break
+#                     elif rules[n][0] < rules2[m+1][0]:
+#                         rules2.insert(m+1,rules[n])
+#                         break
                     
-                elif m+1 == len(rules2):
-                    rules2.append(rules[n])
-                    break
+#                 elif m+1 == len(rules2):
+#                     rules2.append(rules[n])
+#                     break
                     
-                elif rules[n][0] > rules2[m-1][0] and rules[n][0] < rules2[m][0]:
-                    rules2.insert(m,rules[n])
-                    break
-    elif n == 0:
-        rules2.append(rules[0])
+#                 elif rules[n][0] > rules2[m-1][0] and rules[n][0] < rules2[m][0]:
+#                     rules2.insert(m,rules[n])
+#                     break
+#     elif n == 0:
+#         rules2.append(rules[0])
 
-rules3=[rules2[0][0]]
 
-busy = 1
-while busy != 0:
-    busy = 0
-    for n in range(len(rules2)):
-        left = None
-        right = None
-        for m in range(len(rules3)):
-            if rules2[n][0] == rules3[m]:
-                left = m
-            elif rules2[n][1] == rules3[m]:
-                right = m
-        if left is None and right is None:
-            busy +=0
-        elif left is not None and right is None:
-            rules3.insert(left+1,rules2[n][1])
-            busy +=1
-        elif left is None and right is not None:
-            rules3.insert(right,rules2[n][0])
-            busy +=1
-        elif left > right:
-            del rules3[left]
-            rules3.insert(right,rules2[n][0])
-            busy +=1
-    print(busy)
-    input('ok')
+# for n in range(len(rules2)):
+#     left = None
+#     right = None
+#     for m in range(len(rules3)):
+#         if rules2[n][0] == rules3[m]:
+#             left = m
+#         elif rules2[n][1] == rules3[m]:
+#             right = m
+#     if left is None and right is None:
+#         busy +=0
+#     elif left is not None and right is None:
+#         rules3.insert(left+1,rules2[n][1])
+#         busy +=1
+#     elif left is None and right is not None:
+#         rules3.insert(right,rules2[n][0])
+#         busy +=1
+#     elif left > right:
+#         del rules3[left]
+#         rules3.insert(right,rules2[n][0])
+#         busy +=1
+# print(busy)
+# input('ok')
         
+total = 0
 
-print(rules3)
-
-
+for line in lines2:
+    line = line.tolist()
+    busy = 1
+    while busy != 0:
+        busy = 0
+        for n in range(len(rules)):
+            left = 0
+            right = 100
+            for m in range(len(line)):
+                if rules[n][0] == line[m]:
+                    left = m
+                elif rules[n][1] == line[m]:
+                    right = m
+            if left > right:
+                del line[left]
+                line.insert(right,rules[n][0])
+                busy +=1
+        
+    total += line[int((len(line)-1)/2)]
+        
+print(total)
